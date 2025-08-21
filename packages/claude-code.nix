ed300559ae9282de13171874c5a524839def76b1
Maxou Verzou <maxou@integratedintegrals.com>
2025-08-21 09:50:16 -0700

    chore: override claude-code

---
+++ packages/claude-code.nix
@@ -0,0 +1,13 @@
+{
+  claude-code,
+  fetchurl,
+  ...
+}:
+claude-code.override rec {
+  # https://github.com/anthropics/claude-code/issues/6105
+  version = "1.0.81";
+  src = fetchurl {
+    url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
+    hash = "sha256-nlMmdGstMWXYtcIDuLL3ygQEg0cbFeCJakYO8IAerf4=";
+  };
+}
